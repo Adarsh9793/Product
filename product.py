d54bfc64a4a7ce0934bc3925e25a5980bf48c04b
Adarsh tripathi <adarshtripathi9793085@gmail.com>
2025-12-27 23:37:28 +0530

    Add files via upload

---
+++ product.py
@@ -0,0 +1,9 @@
+
+from itertools import product
+
+#A = map(int,input().split())
+#B = map(int,input().split())
+#print(" ".join(str(k) for k in product(A, B)))
+A = [1, 8,2]
+B = [3, 4,5]
+print(" ".join(str(k) for k in product(A, B)))
